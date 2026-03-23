d40080d9f432ea62a0d8d6735fa9aa47d830c570
Nikhil PP <nikhilmac.pp@gmail.com>
2026-03-23 19:42:23 +0530

    Project setup and initial rendering

---
+++ crash-proj/app/page.tsx
@@ -0,0 +1,9 @@
+const Page = () =>{
+  return(
+    <section>
+      <h1 className="text-center">The hub for every dev <br/> Even you can't miss</h1>
+      <p className="text-center mt-5">Hackathons,Meetups and confrences, All in one place</p>
+    </section>
+  )
+}
+export default Page
